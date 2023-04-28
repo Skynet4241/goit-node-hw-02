9f03c55e27ba8de3a6a9194ff78dee3943fa7f4e
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-04-28 11:53:24 +0300

    add validateBody

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -1,17 +1,13 @@
 const contacts = require("../../models/contacts");
 const RequestError = require("../../helpers/requestError");
 
-const removeContact = async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contacts.removeContact(contactId);
-    if (!result) {
-      throw RequestError(404, "Not Found");
-    }
-    res.json({ message: "Contact deleted" });
-  } catch (error) {
-    next(error);
+const removeContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw RequestError(404, "Not Found");
   }
+  res.json({ message: "Contact deleted" });
 };
 
 module.exports = removeContact;
--- controllers/contacts/updateContact.js
+++ controllers/contacts/updateContact.js
@@ -1,22 +1,9 @@
 const contacts = require("../../models/contacts");
-const contactShema = require("../../schemas/contacts");
-const RequestError = require("../../helpers/requestError");
 
-const updateContact = async (req, res, next) => {
-  try {
-    const { error } = contactShema.validate(req.body);
-    if (error) {
-      throw RequestError(400, error.message);
-    }
-    const { contactId } = req.params;
-    const result = await contacts.updateContact(contactId, req.body);
-    if (!result) {
-      throw RequestError(404, "Not Found");
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
+const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+  res.json(result);
 };
 
 module.exports = updateContact;
